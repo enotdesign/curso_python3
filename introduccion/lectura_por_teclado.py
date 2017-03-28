30a2d4573234396df57c46af949ddb1daa18e72c
procreativo <anibalebonilla@gmail.com>
2017-03-28 16:22:04 -0400

    Se actualizo el Quiz

---
+++ introduccion/lectura_por_teclado.py
@@ -0,0 +1,23 @@
+#todas las entradas se guardan como cadenas de caracteres
+valor = input()
+
+valor = input("Introduce un valor: ")
+
+#cambiando valor a un numero con int()
+valor = input("Introduce un numero entero: ")
+valor = int(valor)
+print(valor)
+# otra forma
+valor = int(input("Introduce un numero entero: "))
+print(valor)
+
+#introduciendo numeros flotantes con float()
+valor = input("Introduce un numero flotante: ")
+valor = float(valor)
+print(valor)
+#otra forma
+valor = float(input("Introduce un numero flotante: "))
+print(valor)
+
+
+
