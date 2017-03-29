4ddf4c9dd5f423d45706d5325ddb34ff48c1a156
procreativo <anibalebonilla@gmail.com>
2017-03-29 18:23:11 -0400

    Agregando ejercicios a Operadores logicos

---
+++ Operadores y Expresiones/op_relacionales.py
@@ -0,0 +1,23 @@
+#Igual que 
+print(3 == 2)
+
+#distinto de
+print(3 != 2)
+
+#mayor que
+print(3 > 2)
+
+#meno que
+print(3 < 1)
+
+#mayor o igual que
+print(3 >= 4)
+
+#menor o igual que
+print(3 <= 3)
+
+a = 10
+b = 5
+print(a > b)
+
+print(a == b*2)
---
+++ Operadores y Expresiones/op_tipo_logico.py
@@ -0,0 +1,13 @@
+#es un tipo de dato logico, 
+#Minima Expresion racional
+#representa por dos valores
+#verdadero y Falso
+
+#contexto linguistico
+#::: Estoy vivo <- Es verdadero
+
+#contexto matematico
+#::: 1 + 1 = 3 <- es Falso
+
+print(1+2 ==3) #Igual a falso
+print(1+1 == 2) #igual a verdadero
